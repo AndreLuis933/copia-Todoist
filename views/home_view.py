6f15fa600acf92de9207dab089048692f58f510d
AndreLuis933 <andreluissouza933@gmail.com>
2025-01-12 18:09:44 -0400

    Commit automatico: 12/01/2025 17:39:44,14

--- views/home_view.py
+++ views/home_view.py
@@ -1,6 +1,13 @@
 from flet import *
 from components.slidbar import Slidbar
 from animations.animation import create_animate_slidbar
+from components.adicionar_tarefa import (
+    Calendario,
+    Compartilhado,
+    Tarefa_vencimento,
+    Button_adicionar_tarefa,
+    Card_adicionar_tarefa,
+)
 
 
 class HomeView:
@@ -12,36 +19,39 @@ class HomeView:
         pass
 
     def build(self):
-        main_layout = Row(
-            expand=True,
-            alignment=alignment.top_left,
-            controls=[
-                Slidbar(self.page),
-                ElevatedButton(
-                    "Slidbar",
-                    bgcolor="white",
-                    on_click=self.encontrar,
+
+        calendario = Calendario()
+        compartilhado = Compartilhado()
+        tarefa = Tarefa_vencimento(calendario)
+        button = Button_adicionar_tarefa(compartilhado)
+        card_container = Card_adicionar_tarefa(compartilhado, tarefa)
+
+        content = Stack(
+            [
+                GestureDetector(
+                    on_tap=tarefa.hide_card,
+                    content=Container(expand=True, bgcolor=Colors.TRANSPARENT),
                 ),
+                Row(
+                    expand=True,
+                    alignment=alignment.top_left,
+                    controls=[Slidbar(self.page), Column(
+                    controls=[
+                        button,
+                        card_container,
+                        tarefa,
+                    ],
+                    spacing=20,
+                )],
+                ),
+                
             ],
+            expand=True,
         )
 
-        return main_layout
+        calendario.load_more_months(3)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return content
 
 
 class Teste:
