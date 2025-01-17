7112831dcc5c636e74e2efb7b6bd0652318cd7b9
AndreLuis933 <andreluissouza933@gmail.com>
2025-01-17 13:52:36 -0400

    Commit automatico: 17/01/2025 13:02:36,30

--- antigo/construcao/hora.py
+++ antigo/construcao/hora.py
@@ -1,33 +1,52 @@
 import flet as ft
+from flet import *
 
 def main(page: ft.Page):
     page.window.always_on_top = True 
     
-    container1=ft.Container(
-                width=300,
-                height=300,
-                bgcolor=ft.colors.RED,
-                border_radius=10,
-            )
-    container2=ft.Container(
-                width=250,
-                height=250,
-                bgcolor=ft.colors.GREEN,
-                border_radius=10,
-                left=25,
-                top=25,
-            )
-    container3=ft.Container(
-                width=200,
-                height=200,
-                bgcolor=ft.colors.BLUE,
-                border_radius=10,
-                left=50,
-                top=50,
-            )
+    def on_container_tap(e):
+        if e.control == container3:
+            print("Clicado na camada 3")
+        elif e.control == container2:
+            print("Clicado na camada 2")
+        elif e.control == container1:
+            print("Clicado na camada 1")
+
+    def on_background_tap(e):
+        print("Clicado na camada 0 (fundo)")
+
+    container1 = ft.Container(
+        width=300,
+        height=300,
+        bgcolor=ft.colors.RED,
+        border_radius=10,
+        on_click=on_container_tap
+    )
+    container2 = ft.Container(
+        width=250,
+        height=250,
+        bgcolor=ft.colors.GREEN,
+        border_radius=10,
+        left=25,
+        top=25,
+        on_click=on_container_tap
+    )
+    container3 = ft.Container(
+        width=200,
+        height=200,
+        bgcolor=ft.colors.BLUE,
+        border_radius=10,
+        left=50,
+        top=50,
+        on_click=on_container_tap
+    )
 
     stack = ft.Stack(
         controls=[
+            GestureDetector(
+                on_tap=on_background_tap,
+                content=Container(expand=True, bgcolor=Colors.TRANSPARENT),
+            ),
             container1,
             container2,
             container3,
