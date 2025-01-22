ef856314e477330295dd3f3604a7d410c1f75860
AndreLuis933 <andreluissouza933@gmail.com>
2025-01-22 19:51:03 -0400

    Commit automatico: 22/01/2025 12:41:01,13

--- teste.py
+++ teste.py
@@ -1,6 +1,27 @@
-from ui.components.utils.horarios_em_15_minutos import horarios_15_minutos
+def intervalos_personalizados():
+    intervalos = ['Sem duração']
+    total_minutos = 23 * 60 + 30  # Total de minutos até 23h30
+    minutos = 15
 
+    while minutos <= total_minutos:
+        horas = minutos // 60
+        minutos_restantes = minutos % 60
 
+        if horas > 0 and minutos_restantes > 0:
+            intervalos.append(f"{horas}h{minutos_restantes}m")
+        elif horas > 0:
+            intervalos.append(f"{horas}h")
+        else:
+            intervalos.append(f"{minutos_restantes}m")
+        
+        # Após a primeira hora, os intervalos passam a ser de 30 minutos
+        if minutos < 60:
+            minutos += 15
+        else:
+            minutos += 30
 
-print(horarios_15_minutos())
+    return intervalos
+
+# Testando a função
+print(intervalos_personalizados())
 
