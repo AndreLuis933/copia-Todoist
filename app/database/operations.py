b97c993887a608740a7620446b70a18472b3a15f
AndreLuis933 <andreluissouza933@gmail.com>
2025-01-23 16:37:10 -0400

    Commit automatico: 23/01/2025 16:17:09,80

--- app/database/operations.py
+++ app/database/operations.py
@@ -1,21 +1,37 @@
 from .setup import Tarefa, Session
+from sqlalchemy.exc import SQLAlchemyError
+from contextlib import contextmanager
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise e
+    finally:
+        session.close()
 
 
 def salvar_tarefa(tarefas):
-    tarefa = Tarefa(nome=tarefas[0], descricao=tarefas[1])
-    session = Session()
-    try:
+    with session_scope() as session:
+        tarefa = Tarefa(nome=tarefas[0], descricao=tarefas[1])
         session.add(tarefa)
         session.commit()
         print("Tarefa salva com sucesso!")
-    except Exception as e:
-        print(f"Erro ao salvar tarefa: {e}")
-        session.rollback()
-    finally:
-        session.close()
+
         
 def listar_tarefas():
-    session = Session()
-    tarefas = session.query(Tarefa).all()
-    session.close()
-    return tarefas
+    with session_scope() as session:
+        tarefas = session.query(Tarefa).all()
+        tarefas_lista = []
+        for tarefa in tarefas:
+            tarefa_dict = {
+                'id': tarefa.id,
+                'titulo': tarefa.nome,
+                'descricao': tarefa.descricao,
+            }
+            tarefas_lista.append(tarefa_dict)
+        return tarefas_lista
