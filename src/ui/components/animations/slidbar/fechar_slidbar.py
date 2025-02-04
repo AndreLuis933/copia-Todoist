c4f05aa11f54b9293ff6d0169c5561ba54b4d576
AndreLuis933 <andreluissouza933@gmail.com>
2025-02-03 20:28:19 -0400

    Commit automatico: 03/02/2025 12:48:19,35

--- src/ui/components/animations/slidbar/fechar_slidbar.py
+++ src/ui/components/animations/slidbar/fechar_slidbar.py
@@ -15,6 +15,18 @@ def create_animate_slidbar(page):
 
     def toggle_opacity(controls, show):
         opacity = 1 if show else 0
+                    
+        for item in controls:
+            if isinstance(item, Container) and not item.content is None:
+                if isinstance(item.content, Image):
+                    if 'screen' not in item.content.src:
+                        item.opacity = opacity
+                else:
+                    item.opacity = opacity
+                    
+            item.update()
+
+        time.sleep(0.2)
         for item in controls:
             if isinstance(item, Container) and not item.content is None:
                 if isinstance(item.content, Image):
@@ -23,7 +35,6 @@ def create_animate_slidbar(page):
                 else:
                     item.visible = show
 
-
             item.update()
 
     def animate_slidbar(e):
@@ -31,11 +42,14 @@ def create_animate_slidbar(page):
         if state is None:
             state = SlidbarState()
         slidbar = state.slidbar
+        total = state.titulo_texto.controls
+        total.extend(state.icons_texto.controls[2:])
 
         if state.is_open:
             # Fechar a barra lateral
-            toggle_opacity(state.titulo_texto.controls, False)
-            toggle_opacity(state.icons_texto.controls[2:], False)
+
+            toggle_opacity(total, False)
+            #toggle_opacity(state.icons_texto.controls[2:], False)
             slidbar.bgcolor = Colors.TRANSPARENT
             time.sleep(0.2)
             slidbar.width = 50
@@ -46,8 +60,8 @@ def create_animate_slidbar(page):
             slidbar.height = None
             slidbar.bgcolor = "#222222"
             time.sleep(0.2)
-            toggle_opacity(state.titulo_texto.controls, True)
-            toggle_opacity(state.icons_texto.controls[2:], True)
+            toggle_opacity(total, True)
+            #toggle_opacity(state.icons_texto.controls[2:], True)
 
         state.slidbar.update()
         state.is_open = not state.is_open
